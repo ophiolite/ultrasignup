a4dc500ca766f19fcf61fbb9988b3d797f12ee34
ophiolite <stef@geotarheel.com>
2016-11-11 10:50:53 -0800

    adding morning progress with first buggy version of athlete features python script

---
+++ ult_sign_scrape/athlete_features.py
@@ -0,0 +1,33 @@
+import json
+import sys
+import pandas as pd
+import os
+import requests
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def athlete_features(directory1, directory2):
+
+    for p, dirs, files in os.walk(directory1):
+        for ff in files:
+            print ff
+            athlete = pd.read_json(directory1 + '/' + ff)
+            first = str(athlete['FirstName'])
+            last = str(athlete['LastName'])
+            clean = athlete.drop(['ImageId', 'Status', 'FirstName', 'LastName', 'Results'], axis=1)
+
+            for p, dirs, files in os.walk(directory2):
+                for f in files:
+                    print f
+                    if f == ff + '_clean.csv':
+                        results = pd.read_json(directory2 + '/' + f)
+                        clean['race_total'] = results['status'].count()
+                        clean.to_csv('athlete_features/%s_%s.csv' % (first, last))
+                    else:
+                        print '%s_%s was not written to csv file' % (first, last)
+                        break
+
+
+
+if __name__ == '__main__':
+    athlete_features('racers', 'clean_athlete')
