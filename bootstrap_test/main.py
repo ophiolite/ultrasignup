fab3a30ad20e11e429a99469e015f1828101aa9c
ophiolite <stef@geotarheel.com>
2016-11-25 07:58:53 -0800

    Adding some am progress in cleaning up Flask script

--- bootstrap_test/main.py
+++ bootstrap_test/main.py
@@ -49,32 +49,32 @@ def race_factors():
     '''Takes race from dropdown on webapp in json form and returns dataframe
     with race-specific modeling parameters'''
     race = pd.read_json(race_factors)
-    race_fields = ['Season', 'Metro_area', 'WL_SO', 'Entry_fee', 'PPM']
+
     if race == 'Bear 100':
-        race_features = {'Season': 3, 'Metro_area': 0, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 2.5}
+        race_features = {'Season': [3], 'Metro_area': [0], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [2.5]}
     elif race == 'Black Canyon 100k':
-        race_features = {'Season': 4, 'Metro_area': 1, 'WL_SO': 0, 'Entry_fee': 0, 'PPM': 2.5}
+        race_features = {'Season': [4], 'Metro_area': [1], 'WL_SO': [0], 'Entry_fee': [0], 'PPM': [2.5]}
     elif race == 'Canyons 100k':
-        race_features = {'Season': 1, 'Metro_area': 1, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 3.2}
+        race_features = {'Season': [1], 'Metro_area': [1], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [3.2]}
     elif race == 'Eastern States 100':
-        race_features = {'Season': 2, 'Metro_area': 0, 'WL_SO': 0, 'Entry_fee': 0, 'PPM': 1.85}
+        race_features = {'Season': [2], 'Metro_area': [0], 'WL_SO': [0], 'Entry_fee': [0], 'PPM': [1.85]}
     elif race == 'FatDog 120':
-        race_features = {'Season': 2, 'Metro_area': 0, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 2.7}
+        race_features = {'Season': [2], 'Metro_area': [0], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [2.7]}
     elif race == 'Javelina Jundred':
-        race_features = {'Season': 3, 'Metro_area': 1, 'WL_SO': 0, 'Entry_fee': 1, 'PPM': 2.65}
+        race_features = {'Season': [3], 'Metro_area': [1], 'WL_SO': [0], 'Entry_fee': [1], 'PPM': [2.65]}
     elif race == 'San Diego 100':
-        race_features = {'Season': 2, 'Metro_area': 1, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 2.65}
+        race_features = {'Season': [2], 'Metro_area': [1], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [2.65]}
     elif race == "Sean O'Brien 100k":
-        race_features = {'Season': 4, 'Metro_area': 1, 'WL_SO': 0, 'Entry_fee': 0, 'PPM': 2.5}
+        race_features = {'Season': [4], 'Metro_area': [1], 'WL_SO': [0], 'Entry_fee': [0], 'PPM': [2.5]}
     elif race == 'Tahoe Rim Trail 100':
-        race_features = {'Season': 2, 'Metro_area': 1, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 2.75}
+        race_features = {'Season': [2], 'Metro_area': [1], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [2.75]}
     elif race == 'Western States 100':
-        race_features = {'Season': 2, 'Metro_area': 1, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 4.1}
+        race_features = {'Season': [2], 'Metro_area': [1], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [4.1]}
     elif race == 'Zion 100':
-        race_features = {'Season': 1, 'Metro_area': 1, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 2.5}
+        race_features = {'Season': [1], 'Metro_area': [1], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [2.5]}
     elif race == 'Georgia Death Race':
-        race_features = {'Season': 1, 'Metro_area': 1, 'WL_SO': 1, 'Entry_fee': 1, 'PPM': 3.16}
-    race_factors = pd.concat([race_features,pd.DataFrame(columns=race_fields)])
+        race_features = {'Season': [1], 'Metro_area': [1], 'WL_SO': [1], 'Entry_fee': [1], 'PPM': [3.16]}
+    race_factors = pd.DataFrame(race_features)
     return race_factors
 
 def age_factors(age):
@@ -84,29 +84,29 @@ def age_factors(age):
         age = pd.read_json(int(age))
     except ValueError:
         print("Age is but a number, dude!")
-    age_fields = ['Age', 'runner_rank', 'Age_Rank', 'Gender_Rank', 'Success_rate', 'Total_races']
+
     if age < 20:
-        age_features = {Age: age, runner_rank: 70.3851, Age_Rank: 0.7538, \
-        Gender_Rank: 0.6842, Success_rate, 0.900, Total_races: 11.515}
+        age_features = {Age: [age], runner_rank: [70.3851], Age_Rank: [0.7538], \
+        Gender_Rank: [0.6842], Success_rate, [0.900], Total_races: [11.515]}
     elif age >= 20 and age < 30:
-        age_features = {Age: age, runner_rank: 74.5336, Age_Rank, 0.7254, \
-        Gender_Rank: 0.6560, Success_rate: 0.8798, Total_races, 10.286}
+        age_features = {Age: [age], runner_rank: [74.5336], Age_Rank, [0.7254], \
+        Gender_Rank: [0.6560], Success_rate: [0.8798], Total_races, [10.286]}
     elif age >= 30 and age < 40:
-        age_features = {Age:age, runner_rank: 72.3374, Age_Rank: 0.7319, \
-        Gender_Rank: 0.6689, Success_rate: 0.8867, Total_races: 11.352}
+        age_features = {Age:[age], runner_rank: [72.3374], Age_Rank: [0.7319], \
+        Gender_Rank: [0.6689], Success_rate: [0.8867], Total_races: [11.352]}
     elif age >= 40 and age < 50:
-        age_features = {Age: age, runner_rank: 69.9372, Age_Rank: 0.7308, \
-        Gender_Rank: 0.6668, Success_rate, 0.8753, Total_races: 11.191}
+        age_features = {Age: [age], runner_rank: [69.9372], Age_Rank: [0.7308], \
+        Gender_Rank: [0.6668], Success_rate, [0.8753], Total_races: [11.191]}
     elif age >= 50 and age < 60:
-        age_features = {Age: age, runner_rank: 66.6021, Age_Rank: 0.7339, \
-        Gender_Rank: 0.6733, Success_rate: 0.8837, Total_races: 11.092}
+        age_features = {Age: [age], runner_rank: [66.6021], Age_Rank: [0.7339], \
+        Gender_Rank: [0.6733], Success_rate: [0.8837], Total_races: [11.092]}
     elif age >= 60 and age < 70:
-        age_features = {Age: age, runner_rank: 61.5856, Age_Rank: 0.7213, \
-        Gender_Rank: 0.6683, Success_rate: 0.8721, Total_races: 10.311}
+        age_features = {Age: [age], runner_rank: [61.5856], Age_Rank: [0.7213], \
+        Gender_Rank: [0.6683], Success_rate: [0.8721], Total_races: [10.311]}
     elif age > 70:
-        age_features = {Age: age, runner_rank: 58.3583, Age_Rank: 0.7308, \
-        Gender_Rank: 0.6395, Success_rate: 0.8579, Total_races = 10.080}
-    age_factors = pd.concat([race_features,pd.DataFrame(columns=age_fields)])
+        age_features = {Age: [age], runner_rank: [58.3583], Age_Rank: [0.7308], \
+        Gender_Rank: [0.6395], Success_rate: [0.8579], Total_races = [10.080]}
+    age_factors = pd.DataFrame(age_features)
     return age_factors
 
 @app.route('/', methods=['GET'])
@@ -126,12 +126,14 @@ def Finish_proba():
     age_factors = age_factors(age)
     gender_factors = gender_factors(gender)
     race_factors = race_factors(race)
-    #profile = parse_athlete_json(athlete_answers)
+
     answer_list = [age_factors['Age'], age_factors['runner_rank'], race_factors['Season'], \
     race_factors['Metro_area'], race_factors['WL_SO'], race_factors['Entry_fee'], \
     race_factors['PPM'], age_factors['Age_Rank'], age_factors['Gender_Rank'], \
     race_factors['Total_races'], age_factors['Success_rate'], Gender_F]
+
     probas = Finish_model.predict_proba(answer_list)
+
     return jsonify(probas[0][1]) ##returns the numeric probability of success for selected race
 
 @app.route('/blog', methods=['GET'])
