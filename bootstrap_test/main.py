1fd585a1fc85afe5b022c5ef26d1fb7aa9d62b65
ophiolite <stef@geotarheel.com>
2016-11-25 06:31:44 -0800

    Adding some am progress in cleaning up Flask script

--- bootstrap_test/main.py
+++ bootstrap_test/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, jsonify
 import numpy as np
 import json
 import sys
@@ -7,6 +7,8 @@ import pandas as pd
 import requests
 
 app = Flask(__name__)
+
+@app.endpoint('model.endpoint')
 
 def load_model(filename):
     with open(filename) as f:
@@ -23,7 +25,7 @@ def race_dir_button(filename):
     AUC_score = 0.681
     accuracy_score = 0.956
     acurrate_show = 'Successfully predicted 3015 out of 3016 runners who showed'
-    return AUC_score, accuracy_score, accurate_show ##return when button clicked
+    return jsonify(AUC_score, accuracy_score, accurate_show) ##return when button clicked
 
 
 def parse_athlete_json(answer_list):
@@ -32,141 +34,121 @@ def parse_athlete_json(answer_list):
 ##extrapolate from race: Season, Metro_area, WL_SO, Entry_fee, PPM
 ##gender/age group averages for: runner_rank, Age_Rank, Gender_Rank, Success_rate, Total_races
     '''Need to change reader from list to numpy array'''
-    df = pd.read_json(answer_list)
-    for i, ans in enumerate(answer_list):
-        if ans == 'M':
-            Gender_F = 0.
-        else:
-            Gender_F = 1
-    for i, ans in enumerate(answer_list):
-        if ans == 'BB':
-            Season = 4
-            Metro_area = 1
-            WL_SO = 0
-            Entry_fee = 0
-            PPM = 1.9
-        elif ans == 'Bear 100':
-            Season = 3
-            Metro_area = 0
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 2.5
-        elif ans == 'Black Canyon 100k':
-            Season = 4
-            Metro_area = 1
-            WL_SO = 0
-            Entry_fee = 0
-            PPM = 2.5
-        elif ans == 'Canyons 100k':
-            Season = 1
-            Metro_area = 1
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 3.2
-        elif ans == 'Eastern States 100':
-            Season = 2
-            Metro_area = 0
-            WL_SO = 0
-            Entry_fee = 0
-            PPM = 1.85
-        elif ans == 'FatDog 120':
-            Season = 2
-            Metro_area = 0
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 2.7
-        elif ans == 'Javelina Jundred':
-            Season = 3
-            Metro_area = 1
-            WL_SO = 0
-            Entry_fee = 1
-            PPM = 2.65
-        elif ans == 'San Diego 100':
-            Season = 2
-            Metro_area = 1
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 2.65
-        elif ans == "Sean O'Brien 100k":
-            Season = 4
-            Metro_area = 1
-            WL_SO = 0
-            Entry_fee = 0
-            PPM = 2.5
-        elif ans == 'Tahoe Rim Trail 100':
-            Season = 2
-            Metro_area = 1
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 2.75
-        elif ans == 'Western States 100':
-            Season = 2
-            Metro_area = 1
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 4.1
-        elif ans == 'Zion 100':
-            Season = 1
-            Metro_area = 1
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 2.5
-        elif ans == 'Georgia Death Race':
-            Season = 1
-            Metro_area = 1
-            WL_SO = 1
-            Entry_fee = 1
-            PPM = 3.16
+    pass
+
+def Gender_F():
+    gender = pd.read_json(gender)
+    if ans == 'M':
+        Gender_F = 0.
+    else:
+        Gender_F = 1
+    return Gender_F
+
+def race_factors():
+    race_factors = pd.read_json(race_factors)
+    race_fields = ['Season', 'Metro_area', 'WL_SO', 'Entry_fee', 'PPM']
+    pd.concat([race_features,pd.DataFrame(columns=race_fields)])
+    # if ans == 'BB':
+    #     Season = 4
+    #     Metro_area = 1
+    #     WL_SO = 0
+    #     Entry_fee = 0
+    #     PPM = 1.9
+    if ans == 'Bear 100':
+        race_factors = {Season: 3, Metro_area: 0, WL_SO: 1, Entry_fee: 1, PPM: 2.5}
+    elif ans == 'Black Canyon 100k':
+        race = {Season: 4, Metro_area: 1, WL_SO: 0, Entry_fee: 0, PPM: 2.5}
+    elif ans == 'Canyons 100k':
+        race_factors = Season = 1
+        Metro_area = 1
+        WL_SO = 1
+        Entry_fee = 1
+        PPM = 3.2
+    elif ans == 'Eastern States 100':
+        Season = 2
+        Metro_area = 0
+        WL_SO = 0
+        Entry_fee = 0
+        PPM = 1.85
+    elif ans == 'FatDog 120':
+        Season = 2
+        Metro_area = 0
+        WL_SO = 1
+        Entry_fee = 1
+        PPM = 2.7
+    elif ans == 'Javelina Jundred':
+        Season = 3
+        Metro_area = 1
+        WL_SO = 0
+        Entry_fee = 1
+        PPM = 2.65
+    elif ans == 'San Diego 100':
+        Season = 2
+        Metro_area = 1
+        WL_SO = 1
+        Entry_fee = 1
+        PPM = 2.65
+    elif ans == "Sean O'Brien 100k":
+        Season = 4
+        Metro_area = 1
+        WL_SO = 0
+        Entry_fee = 0
+        PPM = 2.5
+    elif ans == 'Tahoe Rim Trail 100':
+        Season = 2
+        Metro_area = 1
+        WL_SO = 1
+        Entry_fee = 1
+        PPM = 2.75
+    elif ans == 'Western States 100':
+        Season = 2
+        Metro_area = 1
+        WL_SO = 1
+        Entry_fee = 1
+        PPM = 4.1
+    elif ans == 'Zion 100':
+        Season = 1
+        Metro_area = 1
+        WL_SO = 1
+        Entry_fee = 1
+        PPM = 2.5
+    elif ans == 'Georgia Death Race':
+        Season = 1
+        Metro_area = 1
+        WL_SO = 1
+        Entry_fee = 1
+        PPM = 3.16
 ##need something in here to catch numeric from string input
-        Age = i[0]
-        if i[0] < 20:
-            runner_rank = 70.3851
-            Age_Rank = 0.7538
-            Gender_Rank = 0.6842
-            Success_rate = 0.900
-            Total_races = 11.515
-        elif i[0] >= 20 and i[0] < 30:
-            runner_rank = 74.5336
-            Age_Rank = 0.7254
-            Gender_Rank = 0.6560
-            Success_rate = 0.8798
-            Total_races = 10.286
-        elif i[0] >= 30 and i[0] < 40:
-            runner_rank = 72.3374
-            Age_Rank = 0.7319
-            Gender_Rank = 0.6689
-            Success_rate = 0.8867
-            Total_races = 11.352
-        elif i[0] >= 40 and i[0] < 50:
-            runner_rank = 69.9372
-            Age_Rank = 0.7308
-            Gender_Rank = 0.6668
-            Success_rate = 0.8753
-            Total_races = 11.191
-        elif i[0] >= 50 and i[0] < 60:
-            runner_rank = 66.6021
-            Age_Rank = 0.7339
-            Gender_Rank = 0.6733
-            Success_rate = 0.8837
-            Total_races = 11.092
-        elif i[0] >= 60 and i[0] < 70:
-            runner_rank = 61.5856
-            Age_Rank = 0.7213
-            Gender_Rank = 0.6683
-            Success_rate = 0.8721
-            Total_races = 10.311
-        elif i[0] > 70:
-            runner_rank = 58.3583
-            Age_Rank = 0.7308
-            Gender_Rank = 0.6395
-            Success_rate = 0.8579
-            Total_races = 10.080
+def age_factors(age):
 
-    answer_list = [Age, runner_rank, Season, Metro_area, \
-        WL_SO, Entry_fee, PPM, Age_Rank, Gender_Rank, \
-        Total_races, Success_rate, Gender_F]
+        try:
+            age = pd.read_json(int(age))
+        except ValueError:
+            print("Age is but a number, dude!")
 
-    return answer_list
+        if age < 20:
+            age_dict = {Age: age, runner_rank: 70.3851, Age_Rank: 0.7538, \
+            Gender_Rank: 0.6842, Success_rate, 0.900, Total_races: 11.515}
+        elif age >= 20 and age < 30:
+            age_dict = {Age: age, runner_rank: 74.5336, Age_Rank, 0.7254, \
+            Gender_Rank: 0.6560, Success_rate: 0.8798, Total_races, 10.286}
+        elif age >= 30 and age < 40:
+            age_dict = {Age:age, runner_rank: 72.3374, Age_Rank: 0.7319, \
+            Gender_Rank: 0.6689, Success_rate: 0.8867, Total_races: 11.352}
+        elif age >= 40 and age < 50:
+            age_dict = {Age: age, runner_rank: 69.9372, Age_Rank: 0.7308, \
+            Gender_Rank: 0.6668, Success_rate, 0.8753, Total_races: 11.191}
+        elif age >= 50 and age < 60:
+            age_dict = {Age: age, runner_rank: 66.6021, Age_Rank: 0.7339, \
+            Gender_Rank: 0.6733, Success_rate: 0.8837, Total_races: 11.092}
+        elif age >= 60 and age < 70:
+            age_dict = {Age: age, runner_rank: 61.5856, Age_Rank: 0.7213, \
+            Gender_Rank: 0.6683, Success_rate: 0.8721, Total_races: 10.311}
+        elif age > 70:
+            age_dict = {Age: age, runner_rank: 58.3583, Age_Rank: 0.7308, \
+            Gender_Rank: 0.6395, Success_rate: 0.8579, Total_races = 10.080}
+
+    return age_dict
 
 @app.route('/', methods=['GET'])
 def index():
@@ -176,16 +158,27 @@ def index():
 def send_image(path):
     return send_from_directory('images', path)
 
-@app.route('/', methods=['POST'])
+@app.route('/athlete_proba', methods=['POST'])
 def Finish_proba():
     athlete_answers = request.json
-    answer_list = parse_athlete_json(athlete_answers)
+    age, gender, race = (athlete_answers[0],
+                                      athlete_answers[1],
+                                      athlete_answers[2])
+    age_factors = age_factors(age)
+    gender_factors = gender_factors(gender)
+    race_factors = race_factors(race)
+    #profile = parse_athlete_json(athlete_answers)
+    answer_list = [Age, runner_rank, Season, Metro_area, \
+    WL_SO, Entry_fee, PPM, Age_Rank, Gender_Rank, \
+    Total_races, Success_rate, Gender_F]
     probas = Finish_model.predict_proba(answer_list)
-    return probas[0][1] ##returns the numeric probability of success for selected race
+    return jsonify(probas[0][1]) ##returns the numeric probability of success for selected race
 
 @app.route('/blog', methods=['GET'])
 def index():
     return render_template('blog.html')
+
+
 
 if __name__ == '__main__':
     filename = '../model/DNS_model.pkl'
